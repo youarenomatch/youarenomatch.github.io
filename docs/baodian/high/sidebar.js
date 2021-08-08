ee6db96b605c407290bb8acce00febf537c9d39d
“wuning“ <876355950@qq.com>
2021-08-08 10:03:41 +0800

    123

--- docs/baodian/high/sidebar.js
+++
@@ -1,9 +0,0 @@
-module.exports = [
-    {
-        title: '侧测试2',
-        collapsable: true,
-        children: [
-            '/guide/high/notes/one',
-        ]
-    },
-]
--- docs/baodian/zero/sidebar.js
+++
@@ -1,9 +0,0 @@
-module.exports = [
-    {
-        title: '测试1',
-        collapsable: true,
-        children: [
-            '/baodian/zero/notes/one',
-        ]
-    },
-]
